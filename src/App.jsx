114f71e3d956b132e7fd35b08f92d5d7396bcaf0
Loris <45939615+lchastanet@users.noreply.github.com>
2022-05-02 12:05:32 +0200

    Add project basis

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import "./styles/App.css"
+import { useState } from "react"
+import { Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import Profile from "./components/Profile"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
-function App() {
-  return <div className="App"></div>
+export default function App() {
+  const [currentUserName, setCurrentUserName] = useState("")
+  const [currentUserAvatar, setCurrentUserAvatar] = useState("")
+
+  return (
+    <>
+      <Header
+        currentUserAvatar={currentUserAvatar}
+        currentUserName={currentUserName}
+      />
+      <main>
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route
+            path="/profile"
+            element={
+              <Profile
+                currentUserName={currentUserName}
+                setCurrentUserName={setCurrentUserName}
+                setCurrentUserAvatar={setCurrentUserAvatar}
+                currentUserAvatar={currentUserAvatar}
+              />
+            }
+          />
+        </Routes>
+      </main>
+      <Footer currentUserName={currentUserName} />
+    </>
+  )
 }
-
-export default App
